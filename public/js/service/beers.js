924333ffeee7351cca1959ff708a9c6b2c8fe4c2
Mati <matiborlak@gmail.com>
2017-03-05 11:08:15 +0200

    added exmaple beers with ratings, sort button

--- public/js/service/beers.js
+++ public/js/service/beers.js
@@ -7,7 +7,21 @@ app.service('service', function(){
   	abv: "AK",
   	image: "https://c1.staticflickr.com/9/8466/8140544321_75da7d4171.jpg",
     rating: 5
-  }
+  },
+  {
+    name: "Molson's Canadian",
+    style: "Lager",
+    abv: "MOL",
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1d/Molson_Canadian.svg/1042px-Molson_Canadian.svg.png",
+    rating: 2
+  },
+  {
+    name: "Moosehead",
+    style: "Moose piss",
+    abv: "MH",
+    image: "http://moosehead.ca/wp-content/uploads/2015/10/cropped-moosehead_logo-180x180.png",
+    rating: 1
+  },
   ];
 
 
