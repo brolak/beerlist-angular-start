c35da3a7b86a4ba50bc7e57bc69775fd683d60b1
Mati <matiborlak@gmail.com>
2017-03-07 12:56:40 +0200

    new port fix

--- server.js
+++ server.js
@@ -6,7 +6,4 @@ var port = process.env.PORT || 8080;
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
-app.listen(8000, function() {
-  console.log("Fullstack project. Listening on 8000.")
-
-});
+app.listen(port);
