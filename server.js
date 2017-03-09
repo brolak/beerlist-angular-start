66b73d79b06424006f24b9d16d03982f303a431c
Mati <matiborlak@gmail.com>
2017-03-09 09:46:56 +0200

    prep for api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = /*process.env.PORT ||*/ 8080;
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
